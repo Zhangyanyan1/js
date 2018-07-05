85806540d55c40ab9d48f4fc945e35bd4ea94ab1
Zhangyanyan1 <1808548896@qq.com>
2018-07-05 14:33:36 +0800

    Add files via upload

---
+++ shu/js/main.js
@@ -0,0 +1,37 @@
+/**
+ * Created by lenovo on 2018/7/5.
+ */
+var pages = document.getElementsByClassName('page');
+var c = 0, n = 0;
+var sPos = 0,ePos = 0;
+var dir = true;
+document.body.addEventListener('touchstart',function(e){
+    sPos = e.touches[0].pageY;
+});
+document.body.addEventListener('touchend',function(e){
+    isDir();
+});
+document.body.addEventListener('touchmove',function(e){
+    ePos = e.touches[0].pageY;
+});
+function isDir(){
+    if(Math.abs(sPos-ePos) > 30){
+        sPos-ePos > 0?dir = true : dir = false;
+        pageMove();
+    }
+}
+function pageMove(){
+    if(dir){
+        n = c+1;
+        n >= pages.length ? n = 0 : 0;
+        pages[c].className = 'page page'+ c +' toTop';
+        pages[n].className = 'page page'+ n +' toBottom fromBottom';
+        c = n;
+        return;
+    }
+    n = c-1;
+    n < 0 ? n = pages.length - 1 : 0;
+    pages[c].className = 'page page'+ c +' pageToB';
+    pages[n].className = 'page page'+ n +' toBottom pageFromT';
+    c = n;
+}
